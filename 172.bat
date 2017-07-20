de185881a63c723a768765ca37266881cdb7fd73
xiaofengzheng <ilovepeggy88@gmail.com>
2017-07-20 15:53:06 +0800

    combine_bv303b81t_gb_zip.bat, 171 combine_sp9832a_3h10_volte_zip.bat, 172

---
+++ 172.bat
@@ -0,0 +1,26 @@
+::# combine 
+::#    echo "SPRD, bv303c"
+::# call 162
+set unsigned_dir=%2
+set diff_folder=%3
+set signed_folder=%4
+:: signed_folder ends with \ is not recommanded
+set signed_images_dir=%signed_folder%_tmp
+rd /s /q   %signed_folder%
+rd /s /q %signed_images_dir%
+md %signed_folder%
+md %signed_images_dir%
+call 162.bat 162 "nothing_signed" %unsigned_dir%  %diff_folder% %signed_images_dir% true true
+if not "%errorlevel%" == "0" (
+	rd /s /q %signed_images_dir%
+	echo ERROR********************
+	echo      : combine from %unsigned_dir% & %diff_folder% failed
+	exit /b 1
+)
+
+copy /y %unsigned_dir%\* %signed_folder%\
+copy /y %signed_images_dir%\* %signed_folder%\
+::#clean
+rd /s /q %signed_images_dir%
+
+
---
+++ combine_bv303b81t_gb_zip.bat
@@ -0,0 +1,68 @@
+::combine 
+::#    echo "MTK, bv303b"
+::# call 171
+::# combine_bv303b
+::# absolute path is required
+@echo off
+set    unsigned_zip=%1
+set    diff_zip=%2
+set    signed_zip=%3
+set    current_dir=%cd%
+for /f %%a in ("%unsigned_zip%") do (
+	set unsigned_zip_unzip_folder=%temp%\%%~na
+)
+for /f %%a in ("%diff_zip%") do (
+	set diff_zip_unzip_folder=%temp%\%%~na
+)
+for /f %%a in ("%signed_zip%") do (
+	set signed_zip_unzip_folder=%temp%\%%~na
+)
+for /f %%a in ("%current_dir%") do (
+	set current_disk=%%~da
+	set current_folder=%%~fa
+)
+    echo unsigned_zip : %unsigned_zip%
+    echo unsigned_zip_unzip_folder : %unsigned_zip_unzip_folder%
+    echo diff_zip : %diff_zip%
+    echo diff_zip_unzip_folder : %diff_zip_unzip_folder%
+    echo signed_zip : %signed_zip%
+    echo signed_zip_unzip_folder : %signed_zip_unzip_folder%
+    md %unsigned_zip_unzip_folder%
+    md %diff_zip_unzip_folder%
+    md %signed_zip_unzip_folder%
+
+	%current_folder%\winrar.exe x %unsigned_zip% %unsigned_zip_unzip_folder%
+	%current_folder%\winrar.exe x %diff_zip% %diff_zip_unzip_folder%
+	::winrar.exe x %signed_zip% %signed_zip_unzip_folder%
+
+	call 171.bat 171 %unsigned_zip_unzip_folder% %diff_zip_unzip_folder% %signed_zip_unzip_folder%
+	if not "%errorlevel%" == "0" (
+		rd /s /q $unsigned_zip_unzip_folder
+		rd /s /q $diff_zip_unzip_folder
+		rd /s /q $signed_zip_unzip_folder
+		echo ERROR********************
+		echo      : combine from %unsigned_zip% & %diff_zip% failed
+		exit /b 1
+	)
+	del %signed_zip%
+echo     cd %signed_zip_unzip_folder%
+for /f %%a in ("%signed_zip_unzip_folder%") do (
+	set signed_zip_unzip_folder_disk=%%~da
+)
+%signed_zip_unzip_folder_disk%
+    cd %signed_zip_unzip_folder%
+	echo cd:%cd%
+    %current_folder%\winrar.exe a %signed_zip% .\* 
+%current_disk%	
+    cd %current_dir%
+    :: clean
+echo    rd /s /q %unsigned_zip_unzip_folder%
+echo    rd /s /q %diff_zip_unzip_folder%
+echo    rd /s /q %signed_zip_unzip_folder%
+rd /s /q %unsigned_zip_unzip_folder%
+rd /s /q %diff_zip_unzip_folder%
+rd /s /q %signed_zip_unzip_folder%
+    echo COMBINE SIGNED_ZIP DIDD_ZIP OK*****************
+    echo       : combine %unsigned_zip% and %diff_zip% OK
+	
+@echo on	
