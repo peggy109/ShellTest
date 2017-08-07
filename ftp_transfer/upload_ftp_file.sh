020d13e669b0a06566b752cd64fad8a5829b706e
xiaofengzheng <ilovepeggy88@gmail.com>
2017-08-07 11:37:33 +0800

    upload_ftp_file.sh & upload_file.sh

--- ftp_transfer/upload_ftp_file.sh
+++ ftp_transfer/upload_ftp_file.sh
@@ -7,14 +7,39 @@ host=`echo $ftpfile|awk -F "ftp://" '{print $2}'|awk -F "/" '{print $1}'`
 base_path=`dirname $0`
 script_folder=`cd $base_path;pwd`
 password=`/bin/bash $script_folder"/get_ftp_passwd.sh" $host $username`
+if [ "$password" == "" ] ; then
+    echo "*****************"
+    echo "upload  $source to $ftpfile failed"
+    echo "host or username not available"
+    echo "*****************"
+    exit 2010
+fi
 #echo $password
 target=`echo $ftpfile|awk -F "$host" '{print $2}'`
-echo "source: ""$host"
-echo "username: "$username
-echo "source: "$source
-echo "target: "$target
-/bin/bash $script_folder"/get_ftp_passwd.sh" $host $username >> /home/server/get_ftp_passwd_result
-echo /bin/bash $script_folder"/upload_file.sh" $host $username $password $source $target
-/bin/bash $script_folder"/upload_file.sh" $host $username $password $source $target
+echo "**********"
+echo "host:$host"
+echo "port:$port"
+echo "username:$username"
+#echo "password:$password"
+echo "source:$source"
+echo "target:$target"
+echo "**********"
+
+/bin/bash $script_folder"/get_ftp_passwd.sh" $host $username
+echo /bin/bash $script_folder"/upload_file.sh" $host $username $password $source $target 
+/bin/bash $script_folder"/upload_file.sh" $host $username $password $source $target 1> ~/ftp.txt 2>&1
+
+cat ~/ftp.txt | grep -v "Interactive mode off."
+if [ $? -ne 0 ] ; then
+    echo "*****************"
+    echo "upload  $source to $ftpfile successfully"
+    echo "*****************"
+    exit 0
+else
+    echo "*****************"
+    echo "upload  $source to $ftpfile failed"
+    echo "*****************"
+    exit 2020
+fi
 
 
